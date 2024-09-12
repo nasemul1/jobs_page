194cfbcbe4b014ec95b5eafca1bebc3da0099829
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-09-12 19:18:33 +0600

    modified files

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from "@/views/HomeView.vue";
 import Jobs from "@/views/JobsView.vue"
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
             path: '/jobs/:id',
             name: 'job-view',
             component: JobView
+        },
+        {
+            path: '/jobs/add',
+            name: 'add-job',
+            component: AddJobView
         },
         {
             path: '/:catchAll(.*)',
