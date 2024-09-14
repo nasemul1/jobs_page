fe6467ced3540c5fff7955641d5a90ad538d09e4
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-09-14 19:22:37 +0600

    modified files

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,43 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
-  // server: {
-  //   // port: 3000,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://job-page-backend.vercel.app',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://job-page-backend.vercel.app',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //   },
+    // },
+    historyApiFallback: true,  // This ensures fallback routing
   }
-})
+});
+
